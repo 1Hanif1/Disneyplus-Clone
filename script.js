d808e1f77e7beab3a9d9b6f9e55bea82d153bff2
1Hanif1 <hanifmohammed869@gmail.com>
2021-10-30 18:19:36 +0500

    done

--- script.js
+++ script.js
@@ -1,18 +1,27 @@
 'use strict';
+let carouselCounter;
 const carouselContainer = document.querySelector('.carousel-items');
 const carouselImages = document.querySelectorAll('.carousel-items img');
 const leftBtn = document.querySelector('.prevBtn');
 const rightBtn = document.querySelector('.nextBtn');
 const imgWidth = carouselImages[0].clientWidth;
-let carouselCounter;
+const carouselDots = document.querySelector('.dot-container');
 
 window.addEventListener('load', () => {
 	console.log(visualViewport.width);
 	carouselCounter = 1;
-	carouselContainer.style.transform = `translateX(${
-		-imgWidth * carouselCounter + (visualViewport.width < 1024 ? 10 : 60)
-	}px)`;
+	updateCarousel();
+	addDots();
+	updateDots();
 });
+
+setInterval(() => {
+	carouselCounter =
+		carouselCounter === carouselImages.length - 1 ? 0 : ++carouselCounter;
+
+	updateCarousel();
+	updateDots();
+}, 2000);
 
 leftBtn.addEventListener('click', () => {
 	carouselCounter =
@@ -21,6 +30,8 @@ leftBtn.addEventListener('click', () => {
 	carouselContainer.style.transform = `translateX(${
 		-imgWidth * carouselCounter + (visualViewport.width < 1024 ? 10 : 60)
 	}px)`;
+
+	updateDots();
 });
 
 rightBtn.addEventListener('click', () => {
@@ -31,4 +42,24 @@ rightBtn.addEventListener('click', () => {
 	carouselContainer.style.transform = `translateX(${
 		-imgWidth * carouselCounter + (visualViewport.width < 1024 ? 10 : 60)
 	}px)`;
+
+	updateDots();
 });
+
+const addDots = function () {
+	const html = `<div class="dot"></div>`;
+	carouselImages.forEach((_) =>
+		carouselDots.insertAdjacentHTML('afterbegin', html)
+	);
+};
+
+const updateDots = function () {
+	const dots = document.querySelectorAll('.dot');
+	dots.forEach((dot) => (dot.style.backgroundColor = 'hsla(0, 0%, 100%, 0.5)'));
+	dots[carouselCounter].style.backgroundColor = 'hsla(0, 0%, 100%, 1)';
+};
+function updateCarousel() {
+	carouselContainer.style.transform = `translateX(${
+		-imgWidth * carouselCounter + (visualViewport.width < 1024 ? 10 : 60)
+	}px)`;
+}
